22693030d93b80b249f6e418c29523ccf13dc76f
HHHAG58 <WDyxdzszg@163.com>
2022-07-07 15:06:35 +0800

    2022.7.7学习内容

---
+++ test.c
@@ -0,0 +1,164 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//指针进阶
+
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	char* pc = arr;
+//	printf("%s\n", arr);
+//	printf("%s\n", pc);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	const char* p = "abcdef";
+//	//printf("%c\n", *p);//输出首元素地址
+//	//printf("%s\n", p);//根据首元素地址输出字符串
+//
+//	/*报错(无const)
+//	*p = 'W';
+//	printf("%s\n", p);*/
+//	return 0;
+//}
+
+//面试题
+//int main()
+//{
+//	char arr1[] = "abcdef";
+//	char arr2[] = "abcdef";
+//	const char* p1 = "abcdef";
+//	const char* p2 = "abcdef";
+//	/*if (arr1 == arr2)
+//	{
+//		printf("hehe\n");
+//	}
+//	else
+//	{
+//		printf("haha\n");//haha
+//	}*/
+//	if (p1 == p2)
+//	{
+//		printf("hehe\n");//hehe
+//	}
+//	else
+//	{
+//		printf("haha\n");
+//	}
+//	return 0;
+//}
+
+//指针数组
+//int main()
+//{
+//	int arr[10] = { 0 };//整形数组
+//	char ch[5] = { 0 };//字符数组
+//	int* parr[4];//存放整形指针的数组 - 指针数组
+//	char* pch[5];//存放字符指针的数组 - 指针数组
+//	return 0;
+//}
+
+//遍历指针数组输出 - 此方法不常用
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int c = 30;
+//	int d = 40;
+//	int* arr[4] = { &a,&b,&c,&d };
+//	int i = 0;
+//	for (i = 0; i < 4; i++)
+//	{
+//		printf("%d ", *(arr[i]));
+//	}
+//	return 0;
+//}
+
+//常用遍历
+//int main()
+//{
+//	int arr1[] = { 1,2,3,4,5 };
+//	int arr2[] = { 2,3,4,5,6 };
+//	int arr3[] = { 3,4,5,6,7 };
+//
+//	int* parr[] = {arr1,arr2,arr3};
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < 5; j++)
+//		{
+//			printf("%d ", *(parr[i] + j));
+//		}
+//		printf("\n"); 
+//	}
+//	return 0;
+//}
+
+//数组指针 - 指针
+//int main()
+//{
+//	//int* p = NULL;//整形指针 - 指向整形的指针- 可以存放整形的地址
+//	//数组指针 - 指向数组的指针 - 可以存放数组的地址
+//	int arr[10] = { 0 };
+//	//arr - 首元素地址
+//	//&arr[0] - 首元素地址
+//	//&arr - 数组的地址
+//
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int(*p)[10] = &arr;//p就是数组指针 - 此写法不算好
+//
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	char* arr[5];
+//	char* (*pa)[5] = &arr;
+//	return 0;
+//}
+
+//参数是数组的形式
+void print1(int arr[3][5], int x, int y)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < x; i++)
+	{
+		for (j = 0; j < y; j++)
+		{
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+//参数是指针的形式
+void print2(int(*p) [5], int x, int y)
+{
+	int i = 0;
+	for (i = 0; i < x; i++)
+	{
+		int j = 0;
+		for (j = 0; j < y; j++)
+		{
+			printf("%d ", p[i][j]);
+			printf("%d ", *(p[i] + j));
+			//printf("%d ", *(*(p + i) + j));
+			printf("%d ", (*(p + i))[j]);
+		}
+		printf("\n");
+	}
+}
+
+int main()
+{
+	int arr[3][5] = { { 1,2,3,4,5 } ,{ 2,3,4,5,6 } ,{ 3,4,5,6,7 } };
+	print1(arr, 3, 5);
+	print2(arr, 3, 5);
+	return 0;
+}
